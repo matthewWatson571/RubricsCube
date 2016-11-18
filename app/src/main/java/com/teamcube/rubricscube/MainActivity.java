edeec39f2973e400007fdf5cc2916a75376f6b87
jesse-maynard <jesse.maynard@interapthq.com>
2016-11-18 13:19:12 -0500

    merge errors fixed

--- app/src/main/java/com/teamcube/rubricscube/MainActivity.java
+++ app/src/main/java/com/teamcube/rubricscube/MainActivity.java
@@ -1,17 +1,11 @@
 package com.teamcube.rubricscube;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-<<<<<<< HEAD
 import android.widget.RelativeLayout;
-
 import com.davidstemmer.flow.plugin.screenplay.ScreenplayDispatcher;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import flow.Flow;
-=======
->>>>>>> 09d642f62a5c1d048e95fc903bca942d0db9d651
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         ButterKnife.bind(this);
 
         flow = RubricsCubeApplication.getMainFlow();
         dispatcher = new ScreenplayDispatcher(this, container);
         dispatcher.setUp(flow);
-=======
->>>>>>> 09d642f62a5c1d048e95fc903bca942d0db9d651
     }
 }
