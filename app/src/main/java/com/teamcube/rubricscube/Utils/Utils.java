c53778161d8a6b9067df0d23b85c483336fce4ea
Matthew Watson <matthew.watson@interapthq.com>
2016-11-21 13:42:55 -0500

    Jesse-Utils

---
+++ app/src/main/java/com/teamcube/rubricscube/Utils/Utils.java
@@ -0,0 +1,70 @@
+package com.teamcube.rubricscube.Utils;
+
+import android.util.Log;
+
+import java.util.Arrays;
+
+/**
+ * Created by jessemaynard on 11/21/16.
+ */
+
+public class Utils {
+
+
+    public static void printInstructions(String[] splitRub){
+        for (int i = 0; i < splitRub.length; i++){
+            Log.d(">>>>>>>>>>>", splitRub[i]);
+            switch (splitRub[i]){
+                case "F":
+                    break;
+                case "F2":
+                    break;
+                case "F'":
+                    break;
+                case "F'2":
+                    break;
+                case "U":
+                    break;
+                case "U2":
+                    break;
+                case "U'":
+                    break;
+                case "U'2":
+                    break;
+                case "L":
+                    break;
+                case "L2":
+                    break;
+                case "L'":
+                    break;
+                case "L'2":
+                    break;
+                case "R":
+                    break;
+                case "R2":
+                    break;
+                case "R'":
+                    break;
+                case "R'2":
+                    break;
+                case "D":
+                    break;
+                case "D2":
+                    break;
+                case "D'":
+                    break;
+                case "D'2":
+                    break;
+                case "B":
+                    break;
+                case "B2":
+                    break;
+                case "B'":
+                    break;
+                case "B'2":
+                    break;
+            }
+        }
+    }
+
+}
