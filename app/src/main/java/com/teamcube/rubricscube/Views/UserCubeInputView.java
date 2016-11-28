7b9119f1c3765c426106c3686f93ab58c2ad1bfa
Matthew Watson <matthew.watson@interapthq.com>
2016-11-27 23:36:08 -0500

    cleanup

--- app/src/main/java/com/teamcube/rubricscube/Views/UserCubeInputView.java
+++ app/src/main/java/com/teamcube/rubricscube/Views/UserCubeInputView.java
@@ -2112,73 +2112,38 @@ public class UserCubeInputView extends LinearLayout {
     //Solve button and reset click
     @OnClick(R.id.solveButton)
     public void solveCube() {
-//        allCubiesAdded = (orange0Count +
-//                orange1Count +
-//                orange2Count +
-//                orange3Count +
-//                orange4Count +
-//                orange5Count +
-//                orange6Count +
-//                orange7Count +
-//                orange8Count +
-//                green9Count +
-//                green10Count +
-//                green11Count +
-//                green12Count +
-//                green13Count +
-//                green14Count +
-//                green15Count +
-//                green16Count +
-//                green17Count +
-//                white18Count +
-//                white19Count +
-//                white20Count +
-//                white21Count +
-//                white22Count +
-//                white23Count +
-//                white24Count +
-//                white25Count +
-//                white26Count +
-//                red27Count +
-//                red28Count +
-//                red29Count +
-//                red30Count +
-//                red31Count +
-//                red32Count +
-//                red33Count +
-//                red34Count +
-//                red35Count +
-//                blue36Count +
-//                blue37Count +
-//                blue38Count +
-//                blue39Count +
-//                blue40Count +
-//                blue41Count +
-//                blue42Count +
-//                blue43Count +
-//                blue44Count +
-//                yellow45Count +
-//                yellow46Count +
-//                yellow47Count +
-//                yellow48Count +
-//                yellow49Count +
-//                yellow50Count +
-//                yellow51Count +
-//                yellow52Count +
-//                yellow53Count);
+        allCubiesAdded = (
+                // Add the Orange
+                orange0Count + orange1Count + orange2Count + orange3Count +
+                        orange4Count + orange5Count + orange6Count + orange7Count + orange8Count +
+                        // Add the Green
+                        green9Count + green10Count + green11Count + green12Count + green13Count +
+                        green14Count + green15Count + green16Count + green17Count +
+                        // Add the White
+                        white18Count + white19Count + white20Count + white21Count + white22Count +
+                        white23Count + white24Count + white25Count + white26Count +
+                        // Add the Red
+                        red27Count + red28Count + red29Count +
+                        red30Count + red31Count + red32Count + red33Count + red34Count + red35Count +
+                        // Add the Blue
+                        blue36Count + blue37Count + blue38Count + blue39Count + blue40Count + blue41Count +
+                        blue42Count + blue43Count + blue44Count +
+                        // Add the Yellow
+                        yellow45Count + yellow46Count + yellow47Count + yellow48Count + yellow49Count +
+                        yellow50Count + yellow51Count + yellow52Count + yellow53Count);
 
-//        String mixCube =
-//                U1 + U2 + U3 + U4 + U5 + U6 + U7 + U8 + U9 +
-//                        R1 + R2 + R3 + R4 + R5 + R6 + R7 + R8 + R9 +
-//                        F1 + F2 + F3 + F4 + F5 + F6 + F7 + F8 + F9 +
-//                        D1 + D2 + D3 + D4 + D5 + D6 + D7 + D8 + D9 +
-//                        L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9 +
-//                        B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9;
+        String mixCube =
+                U1 + U2 + U3 + U4 + U5 + U6 + U7 + U8 + U9 +
+                        R1 + R2 + R3 + R4 + R5 + R6 + R7 + R8 + R9 +
+                        F1 + F2 + F3 + F4 + F5 + F6 + F7 + F8 + F9 +
+                        D1 + D2 + D3 + D4 + D5 + D6 + D7 + D8 + D9 +
+                        L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8 + L9 +
+                        B1 + B2 + B3 + B4 + B5 + B6 + B7 + B8 + B9;
 
 //TestCube
 
-        String mixCube = "LLFRURLUUFBRBRRRLRDFLUFDDUDLLBFDBUFFBUBDLBRDFUFUDBLDRB";
-        allCubiesAdded = 135;
+//        String mixCube = "LLFRURLUUFBRBRRRLRDFLUFDDUDLLBFDBUFFBUBDLBRDFUFUDBLDRB";
+//        allCubiesAdded = 135;
 
         //If all cubies combined equals 135 then it will show spinner and solve once algorithm is filled in.
         if (allCubiesAdded == 135) {
@@ -2186,7 +2151,7 @@ public class UserCubeInputView extends LinearLayout {
             splitRub = ans.split("\\s+");
 
             Log.d("mixCube", mixCube);
-            Log.d("ans",ans);
+            Log.d("ans", ans);
             Log.d("splitRub", String.valueOf(splitRub));//[Error,8]
 
             Flow flow = RubricsCubeApplication.getMainFlow();
@@ -2196,6 +2161,7 @@ public class UserCubeInputView extends LinearLayout {
 
         } else {
             Toast.makeText(context, (getContext().getString(R.string.cube_error)), Toast.LENGTH_LONG).show();
+
         }
     }
 
